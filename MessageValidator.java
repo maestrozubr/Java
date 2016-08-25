8ed4b7dd30119ead4a4487d147293f1ff8d65173
maestrozubr <artemzubr@mail.ru>
2016-08-25 21:35:04 +0300

    Update MessageValidator.java

--- MessageValidator.java
+++ MessageValidator.java
@@ -3,12 +3,28 @@ import com.google.gson.*;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.*;
 
 
 public class MessageValidator {
     public static void main(String[] args) {
+
+        Dataset datasetEx = new Dataset();
+        datasetEx.param_int = 12;
+        datasetEx.param_string = "string";
+        datasetEx.param_boolean = true;
+        datasetEx.param_double = 12.0;
+        Map<String, String> mapEx = new HashMap<>();
+        mapEx.put("firstName", "Ivan");
+        mapEx.put("lastName", "Ivanov");
+        datasetEx.param_array = mapEx;
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        System.out.println(gson.toJson(datasetEx));
+        String JSONString = gson.toJson(datasetEx);
+
         File f=new File("C:\\Users\\CX\\IdeaProjects\\Brainland\\src\\jsonString.txt");
         try(FileReader reader = new FileReader(f))
         {
@@ -17,17 +33,17 @@ public class MessageValidator {
             reader.read(buffer);
             String JsonString = buffer.toString();
 
-            Dataset dataset = new GsonBuilder()
+            /*Dataset dataset = new GsonBuilder()
                     .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
                     .create()
-                    .fromJson(JsonString, Dataset.class);
+                    .fromJson(JsonString, Dataset.class);*/
 
 
             //System.out.println(JsonString);
 
 
 
-            //parse(JsonString);
+            parse(JSONString);
 
         }
         catch(Exception ex){
@@ -45,18 +61,18 @@ public class MessageValidator {
 
         JsonParser parser = new JsonParser();
         JsonObject jobject = parser.parse(jsonLine).getAsJsonObject();
-        JsonArray jItem = jobject.getAsJsonArray("param_array");
+        System.out.println(jobject.get("param_int").getAsInt());
+        System.out.println(jobject.get("param_string").getAsString());
+        System.out.println(jobject.get("param_boolean").getAsBoolean());
+        System.out.println(jobject.get("param_double").getAsDouble());
 
-        for (JsonElement user : jItem) {
-
-            JsonObject userObject = user.getAsJsonObject();
-            System.out.println(userObject.get("firstName"));
-            System.out.println(userObject.get("lastName"));
-        }
-
+        JsonObject array = jobject.getAsJsonObject("param_array");
+        System.out.println(array.get("firstName").getAsString());
+        System.out.println(array.get("lastName").getAsString());
+       
     }
 
-    public class Dataset {
+    public static class Dataset {
         public int param_int;
         public String param_string;
         public boolean param_boolean;
